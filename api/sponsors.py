7fbd47d7e2edde667e7640cf5a98c668b60f129b
David Ralph <ohlookitsderpy@protonmail.com>
2021-03-18 13:36:30 +0000

    Modify code for use in Mue

--- api/sponsors.py
+++ api/sponsors.py
@@ -6,20 +6,14 @@ from http.server import BaseHTTPRequestHandler
 
 from os import environ, getenv
 
-if environ.get("gh_token") is None:
-    from dotenv import load_dotenv
-    load_dotenv()
-
 def getUsrDetails(u):
-    headers = {"Authorization": f'token {getenv("gh_token")}'}
     reqs = req.get("https://api.github.com/users/"+u).text
     return json.loads(reqs)
 
-def getSponsorNames(u: str):
-    usr = u.split("?u=")[1].split("HTTP")[0].replace(" ", "")
+def getSponsorNames():
     totalUsers = []
     for i in range(1, 1000):
-        url = f'https://github.com/sponsors/{usr}/sponsors_partial?page={i}'
+        url = f'https://github.com/sponsors/davidjcralph/sponsors_partial?page={i}' # todo: get username from env variable
         resp = req.get(url)
         usrCount = 0
         sponsors = 0
@@ -40,16 +34,15 @@ def getSponsorNames(u: str):
             if usrCount == 0:
                 break
 
-        if sponsors == None:
-            d = "Eror: GitHub Sponsors aren't setup with this user."
     d = json.dumps(totalUsers)
     return  '{"sponsors": '+d+"}"
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-        message = getSponsorNames(self.requestline)
+        message = getSponsorNames()
         self.wfile.write(str(message).encode())
         return
